75ce59ee4be201daac395358a48d5e486eebee1c
Teer2008 <henryzimmermann4@gmail.com>
2022-02-26 11:28:19 +0100

    bug fixes

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -1,9 +1,13 @@
-void print(char String){
-    *(char*)0xb8000 = String;
-    return;
+void print(int color, const char *string){
+    volatile char *video = (volatile char*)0xB8000;
+    while( *string != 0 )
+    {
+        *video++ = *string++;
+        *video++ = color;
+    }
 }
 
 extern "C" void main(){
-    print('a');
+    print((255,255,255), "Hello World");
     return;
 }
